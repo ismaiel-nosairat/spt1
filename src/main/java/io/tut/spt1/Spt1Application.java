e19d0db59adb8539247c78c4946910d655f61983
ismaiel-nosairat <ismaiel.nosairat@gmail.com>
2019-09-05 22:00:57 -0700

    initial

---
+++ src/main/java/io/tut/spt1/Spt1Application.java
@@ -0,0 +1,13 @@
+package io.tut.spt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spt1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spt1Application.class, args);
+    }
+
+}
